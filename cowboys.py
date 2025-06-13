3661dff771a69d9d42546e537048c6f240fa028c
skotrla <50790449+skotrla@users.noreply.github.com>
2025-06-12 14:12:18 -1000

    Update cowboys.py

--- cowboys.py
+++ cowboys.py
@@ -313,6 +313,7 @@ match page[0]:
                 with formc:
                     submit = st.form_submit_button("Update Changed Rows")
                     if submit:
+                        print('Test')
                         sql = ''
                         last_update = str(dt.now())[:19]
                         for index, row in ss.edited_df.iterrows():
