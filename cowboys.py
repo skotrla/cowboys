f4f1775f6bac5bf2ccacc4d3529d2e6ab2210a91
skotrla <50790449+skotrla@users.noreply.github.com>
2025-06-12 14:32:48 -1000

    Update cowboys.py

--- cowboys.py
+++ cowboys.py
@@ -331,7 +331,7 @@ match page[0]:
                                 hp = row['High_Price(ea)']
                                 parking_included = row['Parking_Included']
                                 details = row['Details']
-                                stlog(details + '     ' + srow['Details'].tolist()[0])
+                                stlog('old: '+ srow['Details'].tolist()[0] + ', new: ' + details)
                                 if min_qty != srow['Min_Qty'].tolist()[0]:
                                     update = True
                                     if min_qty is None:
