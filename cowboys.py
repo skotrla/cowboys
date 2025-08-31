9bfd3041a464b280dc64b075997791f355c7dfbd
skotrla <50790449+skotrla@users.noreply.github.com>
2025-08-31 09:19:05 -0500

    Update cowboys.py

--- cowboys.py
+++ cowboys.py
@@ -478,9 +478,8 @@ match page[0]:
                         updatedb(f'INSERT INTO newbuyers (Game, Area, Min_Qty, Max_Qty, "Price(ea)", Parking_Included, Details, Buyer, Last_Update, Contact) VALUES ("{game}","{area}",{min_qty},{mq},{price},"{parking_included[0]}","{details}","{name}","{last_update}","{contact}")')
                         modal = Modal(key="OK",title="Listing Added")
                         modal.open()
-                        if modal.is_open():
-                            with modal.container():
-                                st.write('New User Listings Will Not Show Up Until Approved, You Will Be Sent a Login via Contact Info')
+                        with modal.container():
+                            st.write('New User Listings Will Not Show Up Until Approved, You Will Be Sent a Login via Contact Info')
                     else:
                         if price <= 0:
                             st.sidebar.write('Price must be > $0')
@@ -656,6 +655,7 @@ match page[0]:
                             updatedb(sql)
                         else:
                             st.write('Nothing selected')
+
 
 
 
