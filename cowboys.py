eb7cd923dab1bb334aefb7c9d276f12cad0785db
skotrla <50790449+skotrla@users.noreply.github.com>
2025-09-21 12:52:20 -0500

    Update cowboys.py

--- cowboys.py
+++ cowboys.py
@@ -759,8 +759,8 @@ else:
                 st.session_state.hash = hash
                 st_cookie.update('hash')
     cookie = streamlit_js_eval(js_expressions="document.cookie")
-    if cookie.find('cowboys=') > -1:
-        if cookie is not None:
+    if cookie is not None:
+        if cookie.find('cowboys=') > -1:
             cowboys = cookie[cookie.find('cowboys=')+len('cowvboys='):]
             st.session_state.user = cowboys[:cowboys.find('&')]
             st.session_state.hash = cowboys[cowboys.find('&')+1:]
@@ -780,9 +780,3 @@ st.markdown('''<!-- Google tag (gtag.js) --><script async src="https://www.googl
 #st.markdown('<img src="./app/static/giants.jpg">', unsafe_allow_html=True)
 st.title('Dallas Cowboys VETTED Season Ticket Holder Marketplace Web App')
 showpage(page[0])
-
-
-
-
-
-
